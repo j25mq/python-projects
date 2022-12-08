bafcc3fe35242a251dbe2a2bcde1afc5c0553347
julie <juliemiquelard@protonmail.com>
2022-12-08 14:03:32 +0100

    Delete joining.py

--- joining.py
+++
@@ -1,8 +0,0 @@
-lines = ["Haiku frogs in snow",
-         "A limerick came from Nantucket",
-         "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
-
-def breakify(strings):
-    return "<br>".join(strings)
-
-print(breakify(lines))
